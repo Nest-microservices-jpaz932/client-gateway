633b2f1db2f73a16d1a7b09f3c5d3d4704f6d02b
jpaz932 <juanpa.aristis@gmail.com>
2025-05-19 23:12:45 -0500

    feat: add HealthCheck module and controller, and update AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthCheckModule } from './health-check/health-check.module';
 
 @Module({
-    imports: [ProductsModule, OrdersModule, AuthModule],
+    imports: [ProductsModule, OrdersModule, AuthModule, HealthCheckModule],
     controllers: [],
     providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
@@ -9,7 +9,14 @@ async function bootstrap() {
     const logger = new Logger('Main GateWay');
     const app = await NestFactory.create(AppModule);
 
-    app.setGlobalPrefix('api');
+    app.setGlobalPrefix('api', {
+        exclude: [
+            {
+                path: '',
+                method: RequestMethod.GET,
+            },
+        ],
+    });
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
